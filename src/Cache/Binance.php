224a3c3d6edb38f1c3b558c199fa7e46949190ee
kleninmaxim <maksim_klenin_99@mail.ru>
2022-04-28 22:08:47 +0300

    Fix second for cache

--- src/Cache/Binance.php
+++ src/Cache/Binance.php
@@ -31,7 +31,7 @@ trait Binance
 
         $time_interval = KlineInterval::timeframeInSeconds($interval);
 
-        $seconds = $time_interval - time() % $time_interval - 1;
+        $seconds = $time_interval - time() % $time_interval - 2;
 
         return ($seconds < 86400) ? $seconds : 86400;
 
