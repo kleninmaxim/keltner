cd277ba7d26d70f8a2fbc18de2d2d2512c9f9848
kleninmaxim <maksim_klenin_99@mail.ru>
2022-05-02 18:15:59 +0300

    Add cache function fetchKlinesFuturesCache

--- src/Cache/Binance.php
+++ src/Cache/Binance.php
@@ -8,6 +8,24 @@ use Src\Exchange\KlineInterval;
 trait Binance
 {
 
+    public function fetchKlinesFuturesCache(string $symbol, string $interval, int $limit = 1000): mixed
+    {
+
+        return Cache::remember(
+            $symbol . $interval . $limit . '_futures',
+            fn () => $this->getSecondsForCache($interval),
+            function () use ($symbol, $interval, $limit) {
+                return $this->fetchKlinesFutures(
+                    $symbol,
+                    $interval,
+                    $limit,
+                    removeCurrent: true
+                );
+            }
+        );
+
+    }
+
     public function fetchKlinesCache(string $symbol, string $interval, int $limit): mixed
     {
 
